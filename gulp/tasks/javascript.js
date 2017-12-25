1a63266dd075806da6d9d3c913ce077ad976c547
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2017-12-25 18:11:32 +0800

    init

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -6,22 +6,31 @@ const rimraf = require('rimraf');
 const concat = require('gulp-concat-util');
 
 gulp.task('js.clean', function (cb) {
-    return rimraf(config.js.dest, cb);
+  return rimraf(config.js.dest, cb);
 });
 
-gulp.task('js.vendor', function() {
-  return gulp.src(
-      [config.js.entry + '*.js'])
-    .pipe(concat('video.js'))
+gulp.task('js.vendor', function () {
+  return gulp.src([
+    config.js.entry + 'vendor/swiper-3.4.1.jquery.min.js',
+    config.js.entry + 'vendor/swiper.animate1.0.2.min.js',
+    config.js.entry + 'vendor/fastclick.js',
+    config.js.entry + 'vendor/countUp.min.js',
+    config.js.entry + 'vendor/wScratchPad.js'
+  ])
+    .pipe(concat('vendor.js'))
     .pipe(gulp.dest(config.js.dest));
 });
-gulp.task('js.app', function(){
-
+gulp.task('js.app', function () {
+  return gulp.src([
+    config.js.entry + 'app/app.js',
+  ])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(config.js.dest));
 });
 gulp.task('js.build', function () {
-    runSequence(
-        'js.clean',
-        'js.vendor',
-        ['browserSync.reload', 'js.app']
-    );
+  runSequence(
+    'js.clean',
+    'js.vendor',
+    ['browserSync.reload', 'js.app']
+  );
 });
